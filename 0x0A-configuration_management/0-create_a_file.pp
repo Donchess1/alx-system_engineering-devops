ca8c5c000398f9fcddd09cd84e49953031419896
Donchess1 <Stepabod@yahoo.com>
2024-01-19 20:48:04 +0100

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,9 @@
-file {'/tmp/school':
-    mode       => '0744',
-    owner      => www-data,
-    group      => www-data,
-    content    => 'I love Puppet'
+# This puppet manifest creates a file names/tmp/school
+file { '/tmp/school':
+  ensure         => file,
+  mode           => '0744',
+  owner          => 'www-data',
+  group          => 'www-data',
+  content        => 'I love Puppet\n',
+  ensure_newline => true,
 }
