6f26a0dc1e7521f8dafdae2156153ea004ddfaab
Donchess1 <Stepabod@yahoo.com>
2024-01-23 22:34:52 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,14 @@
 include stdlib
 
 file_line { 'Turn off passwd auth':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Declare identity file':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '     IdentityFile ~/.ssh/school',
   replace => true,
