a19530cd55d26cf83bde8d9b020cc1b746f0114d
Donchess1 <Stepabod@yahoo.com>
2024-03-15 07:55:14 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,4 +2,4 @@
 exec { 'fix--for-nginx':
   command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
 /etc/default/nginx; service nginx restart\"",
-  path    => '/usr/bin:/usr/sbin:/bin'
+  path    => '/usr/bin:/usr/sbin:/bin'}
